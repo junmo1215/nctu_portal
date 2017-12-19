e82a094327f2b7d6af987e040b35bdf341e64b8e
ljm1992 <997128120@qq.com>
2017-12-19 21:03:24 +0800

    处理完成第一种验证码

---
+++ training/helper.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+import os
+import uuid
+from PIL import Image
+
+def filter_img(img, threshold=170):
+    '对于黑白图像，将像素值大于某一个值的部分变成白色，小于这个值的部分变成黑色'
+    filtered = Image.new(img.mode, img.size)
+    for x in range(0, img.size[0]):
+        for y in range(0, img.size[1]):
+            if img.getpixel((x, y)) > threshold:
+                filtered.putpixel((x, y), 255)
+            else:
+                filtered.putpixel((x, y), 0)
+    return filtered
+
+def random_name():
+    return "{}.jpg".format(uuid.uuid4().hex)
